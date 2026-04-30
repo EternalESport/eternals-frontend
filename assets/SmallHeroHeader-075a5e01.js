2060524db63f444d71459500bf02a16796e37022
Mikael G. Laferrière <mikaelglaferriere@gmail.com>
2026-04-30 11:48:58 -0400

    Updates

--- assets/SmallHeroHeader-075a5e01.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,o as a,c as o,b as s,t as _,p as c,f as r}from"./index-0fe32c13.js";const p=e=>(c("data-v-f6915470"),e=e(),r(),e),d={class:"greetings"},l=p(()=>s("div",{class:"fade"},null,-1)),n={__name:"SmallHeroHeader",props:{msg:{type:String,required:!0}},setup(e){return(i,m)=>(a(),o("div",d,[s("h1",null,_(e.msg),1),l]))}},S=t(n,[["__scopeId","data-v-f6915470"]]);export{S};
